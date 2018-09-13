ae5989aa0cd4d8e67642b98a9be8dbfb3533b482
MagnusAndreasen <31634063+MagnusAndreasen@users.noreply.github.com>
2018-09-13 06:08:00 +0200

    added modal to buttons
    added modal to "add post it" and "login/register" buttons.

--- post-it/modaltest.php
+++ post-it/modaltest.php
@@ -2,33 +2,16 @@
 session_start();
 require_once('util.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Bootstrap - Prebuilt Layout</title>
-
-<!-- Bootstrap -->
-<link href="css/bootstrap-4.0.0.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
-<!-- Custom Styles -->
-<link href="css/styles.css" rel="stylesheet">
+<title>Untitled Document</title>
+ <link href="../post-it/css/bootstrap-4.0.0.css" rel="stylesheet">
 
 </head>
+
 <body>
-<div class="container-fluid">
-  <div class="jumbotron jumbotron-fluid text-center">
-    <h1 class="display-4">Your Post-It Wall</h1>
-    <p class="lead">Easily manage your notes!</p>
-    <hr class="my-4">
-    <p>Press button below to either make a new post-it or register.</p>
-	<!-- Button trigger modal -->
-	<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#exampleModal">Add Post-It</button>
-	<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#testModal">Login/Register</button>
-  </div>
-	
 <!-- Add post-it Modal -->
 <div class="modal fade" id="testModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
@@ -89,7 +72,8 @@ require_once('util.php');
     </div>
   </div>
 </div>	
-<!-- Add post-it Modal END -->	
+	
+	
 <!-- Login Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
@@ -133,67 +117,13 @@ require_once('util.php');
       </div>
     </div>
   </div>
-</div>
-<!-- Login Modal END -->
-			
-</div>
-<div class="container">
-  <div class="row text-center">
-    <div class="col-lg-6 offset-lg-3">See all post-it notes below. <br>
-      <strong>Login</strong> to see the private notes.</div>
-  </div>
-</div>
-  <br>
-  <hr>
-  <br>
-  <div class="container-fluid">
-	<!--Posters kasse-->  
-		<div class="row justify-content-center">
-				<?php 
-					require_once('dbcon.php');
-					$sql = 'SELECT postit.id, createdate, author, headertext, bodytext, cssclass FROM postit, color WHERE color_id=color.id';
-
-					$stmt = $link->prepare($sql);
-					$stmt->execute();
-					$stmt->bind_result($pid, $createdate, $author, $htext, $btext, $cssclass);
-
-					while($stmt->fetch()){ ?>	
-					<div class="card postit>">
-			  			<div class="card-body zoom">
-							<div class="<?=$cssclass?>">
-								<form action="dodeletepostit.php" method="post" >
-									<input type="hidden" name="pid" value="<?=$pid?>">
-									<input type="image" src="pic/lillex.png" alt="Delete" class="slet-poster">
-								</form>		
-								<p class="overskrift"><?=$htext?></p>
-								<p class="tekst"><?=$btext?></p>
-								<p class="author"><?=$author?></p>
-								<p class="dato"><?=$createdate?></p>
-							</div>
-						</div>
-					</div>
-				<?php	
-				} ?>
-  		</div>
-	<!--Posters kasse slut-->
-  <br>
-  <hr>
-  <div class="row">
-    <div class="text-center col-lg-6 offset-lg-3">
-      <h4>Your Post-It Wall </h4>
-      <p>Copyright &copy; 2018 &middot; All Rights Reserved &middot; <a href="#" >PMS</a></p>
-    </div>
-  </div>
-</div>
+</div>		
 	
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-<script src="js/jquery-3.2.1.min.js"></script> 
+<script src="../post-it/js/jquery-3.2.1.min.js"></script> 
 
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="js/popper.min.js"></script> 
-<script src="js/bootstrap-4.0.0.js"></script>
-<!-- custom script) --> 	
+<script src="../post-it/js/popper.min.js"></script> 
+<script src="../post-it/js/bootstrap-4.0.0.js"></script>	
 <script src="customjs.js"></script>
 </body>
 </html>
